ca00f7df13c096a9f8b344d53062f2af9b69673c
Simon Stone <sstone1@uk.ibm.com>
2020-01-31 09:23:37 +0000

    Add extension skeleton
    Signed-off-by: Simon Stone <sstone1@uk.ibm.com>

---
+++ src/extension.ts
@@ -0,0 +1,9 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {}
+
+export function deactivate() {}
